9ccdcf6b3926aeec27fa2a917b0ebe515dc1a379
abisheksivaraj <abisheksivaraj2001.com>
2025-04-20 16:29:18 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import AdminPage from "./Components/AdminPage";
-import Dashboard from "./Components/Dashboard";
-import IssueMaster from "./Components/IssueMaster";
-import ColorCodeMaster from "./Components/ColorCodeMAster";
-import AddEmployee from "./Components/AddEmployee";
-import AdminProcess from "./Components/AdminProcess";
+
 import Login from "./Components/Login";
 import { Route, Routes, useLocation } from "react-router-dom";
-
 
 const App = () => {
   const location = useLocation();
